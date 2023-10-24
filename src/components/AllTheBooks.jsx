e3c4f5067fee790eaaab19462f1d4a81c085f0a7
IsuruFerna <isurumadushan.ferna@gmail.com>
2023-10-24 17:13:06 +0200

    card size partially fixed

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -11,9 +11,13 @@ const AllTheBooks = function (prop) {
          <Row className="row-cols-2 row-cols-md-3 row-cols-lg-4">
             {prop.bookGenre.map((book) => {
                return (
-                  <Col key={book.asin}>
-                     <Card>
-                        <Card.Img variant="top" src={book.img} />
+                  <Col key={book.asin} className="my-3">
+                     <Card className="h-100">
+                        <Card.Img
+                           variant="top"
+                           className="h-75"
+                           src={book.img}
+                        />
                         <Card.Body>
                            <Card.Title>{book.title}</Card.Title>
                            <Card.Text>Price: $ {book.price}</Card.Text>
